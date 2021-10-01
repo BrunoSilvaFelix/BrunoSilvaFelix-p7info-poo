576373513cae532cbff7c8b724ebe35110621950
Bruno da Silva <bruno.silva.felix08@aluno.ifce.edu.br>
2021-10-01 17:24:41 -0300

    Alterado em 01/10/2021

--- atividade-avaliação/Avaliação-04/PontoReta.py
+++ atividade-avaliação/Avaliação-04/PontoReta.py
@@ -1,45 +1,50 @@
-"""Escreva em Python uma classe Ponto que possui os atributos inteiros x e y. 
-Escreva uma classe Reta que possui dois pontos a e b. 
-Escreva os métodos construtores para a classe Ponto e para a Classe Reta. 
-Escreva os métodos get e set para acessar e alterar os atributos da classe Ponto e da classe Reta. 
-Escreva um método distancia que retorna um valor real da distancia entre os dois pontos da reta."""
 
-class Ponto:
-    def __init__(self, x, y):
-        self.__x = int(x)
-        self.__y = int(y)
-        
-    def set_x(self, x):
-        self.__x = x
+class Ponto():
+    def __init__(self, x , y):
+        self.x = x
+        self.y = y
     
-    def get_x(self):
-        return self.__x
+    def set_X(self, x):
+        self.x = x
     
-    def set_y(self, y):
-        self.__y = y
+    def set_Y(self, y):
+        self.y = y
     
-    def get_y(self):
-        return self.__y
-    
-class Reta:
+    def get_X(self):
+        return self.x
+
+    def get_Y(self):
+        return self.y
+
+class Reta():
     def __init__(self, pontoA, pontoB):
         self.pontoA = pontoA
         self.pontoB = pontoB
-        
+
     def set_pontoA(self, pontoA):
-        self.pontoA = pontoA.Ponto()
+        self.pontoA = pontoA
+
+    def set_pontoB(self, pontoB):
+        self.pontoB = pontoB
     
     def get_pontoA(self):
-        return self.pontoA.Ponto()
-    
-    def set_pontoB(self, pontoB):
-        self.pontoB = pontoB.Ponto()
+        return self.pontoA
     
     def get_pontoB(self):
-        return self.pontoB.Ponto()
-    
+        return self.pontoB
+
     def distancia(self):
-        d = ()
-    
-    
-    
+        parteX = (self.get_pontoB().get_X() - self.get_pontoA().get_X())**2
+        parteY = (self.get_pontoB().get_Y() - self.get_pontoA().get_Y())**2
+        resultado = (parteX + parteY) ** 0.5
+        
+        return resultado
+
+
+pontoA = Ponto(0, 0)
+pontoB = Ponto(3, 4)
+
+retaA = Reta(pontoA,pontoB)
+
+print("A distância entre os pontos A e B é", retaA.distancia())
+
