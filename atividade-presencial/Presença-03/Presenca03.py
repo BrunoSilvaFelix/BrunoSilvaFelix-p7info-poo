b242f11b6a3b6fd71829f5c907824bbf33bcd581
Bruno da Silva <bruno.silva.felix08@aluno.ifce.edu.br>
2021-10-02 10:55:14 -0300

    Alterado em 02/10/2021

--- atividade-presencial/Presença-03/Presenca03.py
+++ atividade-presencial/Presença-03/Presenca03.py
@@ -1,1 +1,39 @@
-teste = "Nada"
+"""
+1) Criar uma Pilha Vazia;
+2) Inserir um elemento na Pilha (a inserção é pelo topo da Pilha);
+3) Retirar um elemento da Pilha (a retirada é pelo topo da Pilha);  
+4) Mostrar a Pilha.
+"""
+
+class Pilha():
+    
+    def __init__(self):
+       self.pilha = []
+       
+    def adicionar(self, elemento):
+       self.pilha.append(elemento)
+    
+    def retirar(self):
+        if len(self.pilha) > 0:
+            return self.pilha.pop(-1)
+    
+    def vazio(self):
+        return self.pilha == []
+    
+    def mostrarPilha(self):
+        print(self.pilha)
+       
+    
+stack = Pilha()
+
+for i in range(10):
+    stack.adicionar(i)
+
+stack.mostrarPilha()
+stack.retirar()
+stack.mostrarPilha()
+
+for i in range(10):
+    stack.retirar()
+
+stack.mostrarPilha()
