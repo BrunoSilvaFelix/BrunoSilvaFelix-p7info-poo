970113a95d28ee40370cc7eaccf07ec02dbed844
Bruno da Silva <bruno.silva.felix08@aluno.ifce.edu.br>
2021-10-11 16:47:18 -0300

    Alterado

--- atividade-avaliação/Avaliação-02/Avaliacao02.py
+++ atividade-avaliação/Avaliação-02/Avaliacao02.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 x = 0
 car = "-"
 while True:
@@ -18,24 +16,5 @@ while True:
 print()
 print("A maior palavra é: %s" % maiorPalavra)
 
-=======
 
-x = 0
-car = "-"
-while True:
-    print("Frase:")
-    frase = input().split()
-    tamanho = []
-    if frase == ['0']:
-        break
-    for i in frase:
-        tamanho.append(str(len(i)))
-        if len(i) >= x:
-            x = len(i)
-            maiorPalavra = i
-    print(car.join(tamanho))
 
-print()
-print("A maior palavra é: %s" % maiorPalavra)
-
->>>>>>> main
